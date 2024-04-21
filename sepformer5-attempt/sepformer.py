d0c18425b504ad22e0058afe5f42137a1cd05e25
Alexey Slizkov <elexunix@gmail.com>
2024-04-22 00:59:10 +0300

    In order to get model not trained, we simply don't freeze it, unlike the authors of Pretrained

---
+++ sepformer5-attempt/sepformer.py
@@ -0,0 +1,32 @@
+import torch, torch.nn as nn
+import speechbrain.pretrained
+from hyperpyyaml import load_hyperpyyaml
+
+
+#model = speechbrain.pretrained.SepformerSeparation.from_hparams(source='speechbrain/sepformer-wsj03mix', savedir='pretrained-models/sepformer-wsj03mix')
+
+hparams = load_hyperpyyaml(open('hyperparams.yaml'))
+#pretrained = hparams['pretrainer']
+#speechbrain.pretrained.SepformerSeparation(hparams['modules'], hparams)
+
+
+class Pretrained(nn.Module):
+  def __init__(self, modules, hparams):
+    super().__init__()
+    self.mods = nn.ModuleDict(modules)
+    self.device = 'cpu'
+    for module in self.mods.values():
+      if module is not None:
+        module.to(self.device)
+
+
+class SepformerModel(Pretrained):
+  def separate_batch(self, mix):
+    raise NotImplementedError("speechbrain/pretrained/interfaces.py:2184")
+  def forward(self, mix):
+    return self.separate_batch(mix)
+
+
+model = SepformerModel(modules=hparams['modules'], hparams=hparams)
+print('model with', sum(p.numel() for p in model.parameters()), 'parameters')
+print('training:', model.training, '!!')
