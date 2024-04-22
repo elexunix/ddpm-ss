1aa8acdeebf68a69c6ef779851b3126c0a3ab4f9
Alexey Slizkov <elexunix@gmail.com>
2024-04-22 11:07:37 +0300

    Continue integrating Sepformer5

--- sepformer5-attempt/sepformer.py
+++ sepformer5-attempt/sepformer.py
@@ -1,21 +1,3 @@
-#from speechbrain.pretrained import SepformerSeparation as sepformer
-from .interfaces import SepformerSeparation5
-import torchaudio
-
-
-class Sepformer5Model():
-  def __init__(self):
-    self.resampler_16k8k = torchaudio.transforms.Resample(orig_freq=16000, new_freq=8000)
-    self.resampler_8k16k = torchaudio.transforms.Resample(orig_freq=8000, new_freq=16000)
-    self.model = SepformerSeparation5.from_hparams(source='speechbrain/sepformer-wsj03mix', savedir='pretrained-models/sepformer-wsj03mix')
-
-  def forward(self, mixture_at_16khz):
-    mixture_at_8khz = self.resampler16k8k(mixture_at_16khz)
-    est_sources = self.model(mixture_at_8khz)
-    assert est_sources.ndim == 3
-    assert est_sources.shape[-1] == 5  # here are the 5 speakers
-    predicted = self.resampler_8k16k(est_sources.permute(-1, 0, 1))
-    return {f'predicted{i}' : predicted[i] for i in range(5)}
 import torch, torch.nn as nn
 import speechbrain.pretrained
 from hyperpyyaml import load_hyperpyyaml
@@ -39,12 +21,14 @@ class Pretrained(nn.Module):
 
 
 class SepformerModel(Pretrained):
+  def __init__(self, hparams):
+    super().__init__(modules=hparams['modules'], hparams=hparams)
   def separate_batch(self, mix):
     raise NotImplementedError("speechbrain/pretrained/interfaces.py:2184")
   def forward(self, mix):
     return self.separate_batch(mix)
 
 
-model = SepformerModel(modules=hparams['modules'], hparams=hparams)
+model = SepformerModel(hparams)
 print('model with', sum(p.numel() for p in model.parameters()), 'parameters')
 print('training:', model.training, '!!')
